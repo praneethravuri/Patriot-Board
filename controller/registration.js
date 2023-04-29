4dcc151c236adacb09c7f1078e924d092581abb2
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-29 05:19:25 -0400

    responsive registration.html

--- controller/registration.js
+++ controller/registration.js
@@ -1,74 +1,105 @@
-/*function showPWField() {
-    var id = document.getElementById("AdvancedSearch");
-    if (id.style.display === "none") {
-        id.style.cssText = "display: flex; flex-direction:column; min-height: 20%; justify-content: space-between;";
-    } else {
-        id.style.display = "none";
-    }
-}
-*/
-
-$("#AdvancedSearch").hide();
-
-$(document).ready(function(){
-    $("#advanced-option").click(function(){
-        $("#AdvancedSearch").show();
-        document.getElementById("AdvancedSearch").style.cssText = "display: flex; flex-direction:column; min-height: 20%; justify-content: space-between;";
-    })
-})
-
 async function readJSONFile(url) {
     const data = await $.getJSON(url);
     return data;
 }
 
 let courseList = await readJSONFile("./model/courseList.json");
+let studentData = await readJSONFile("./model/studentData.json");
+let studentDetails = await readJSONFile("./model/credentials.json");
 
-//module.exports = readJSONFile;
+let courseDetails = courseList;
 
-$(document).ready(function(){
-    $("#registered-courses-table").hide();
-    $("#courses-status").text("You have not registered for any courses");
-});
 
-function showResults(course, courseList){
-    document.getElementById("results").style.display = "block";
-    $(document).ready(function(){
-        let row = "<tr>"
-        row += "<td>" + course + "</td>"
-        row += "<td>" + courseList[course].instrName + "</td>"
-        row += "<td>" + courseList[course].location + "</td>"
-        row += "<td>" + courseList[course].timings + "</td>"
-        $("#search-result-course").append(row);
-    });
+
+const courseSelect = document.getElementById("courseSelect");
+const addedCourses = [];
+
+for (const course in courseList) {
+  const courseCode = course.split(" ")[0];
+  if (!addedCourses.includes(courseCode)) {
+    addedCourses.push(courseCode);
+    const option = document.createElement("option");
+    option.value = courseCode;
+    option.text = courseCode;
+    courseSelect.add(option);
+  }
 }
+const courseNumberInput = document.getElementById("courseNumberInput");
+const searchResults = document.getElementById("searchResults");
+
+// Event listeners
+courseSelect.addEventListener("change", showCourseDetails);
+courseNumberInput.addEventListener("keyup", showCourseDetails);
 
 
 
-$(document).ready(function(){
-    $("#search-btn").click(function(){
-        let course = ($("#dcode").val().trim() + " " + $("#cno").val().trim()).toUpperCase();
-        if(course in courseList){
-            showResults(course, courseList);
-        }
-        else{
-            $("#search-result").text("This course is not available");
-        }
-    });
-});
+// Function to show course details
+// Function to show course details
+function showCourseDetails() {
+    // Clear any previous cards
+    searchResults.innerHTML = "";
+    $("#available-courses-title").show();
+  
+    // Get the selected course option
+    const selectedOption = courseSelect.value !== "none" ? courseSelect.value : "";
+  
+    // Get the entered course number
+    const courseNumber = courseNumberInput.value.trim().toUpperCase();
+  
+    // Loop through each course and check if it contains the selected option and/or course number
+    for (const course in courseList) {
+      const courseCode = course.split(" ")[0].toUpperCase();
+      const courseNum = course.split(" ")[1].toUpperCase();
+  
+      if (courseCode.includes(selectedOption) && (courseNumber === "" || courseNum.startsWith(courseNumber))) {
+        // Create a card with course details
+        const card = document.createElement("div");
+        card.className = "col-md-12 mb-3";
+        card.innerHTML = `
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${course}</h5>
+              <p class="card-text">${courseList[course].description}</p>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Instructor: ${courseList[course].instrName}</li>
+                <li class="list-group-item">Location: ${courseList[course].location}</li>
+                <li class="list-group-item">Timings: ${courseList[course].timings}</li>
+                <li class="list-group-item"><button class="${courseCode}-${courseNum} select-course-btn">Select - ${course}</button></li>
+              </ul>
+            </div>
+          </div>
+        `;
+        searchResults.appendChild(card);
+      }
+    }
+  }
 
-$(document).ready(function(){
-    $("#add-class").click(function(){
-        addAllRows();
-    });
-});
-
-function addAllRows(){
-    $("#registered-courses-table").find("tr:gt(0)").remove();
-    $("#courses-status").text("Registered Courses");
-    $("#registered-courses-table").show();
-    $("#search-result-course tr").each(function(){
-        $("#registered-courses-table-body").append($(this).clone());
-    });
-}
-
+// Event listener for search results
+searchResults.addEventListener("click", function(event) {
+    if (event.target.classList.contains("select-course-btn")) {
+        const courseCodeNum = event.target.classList[0].split("-");
+        const courseCode = courseCodeNum[0];
+        const courseNum = courseCodeNum[1];
+        const selectedCourse = courseList[`${courseCode} ${courseNum}`];
+  
+      // Create a card for the selected course
+      const card = document.createElement("div");
+      card.className = "col-md-4 mb-3";
+      card.innerHTML = `
+        <div class="card custom-card">
+          <div class="card-body">
+            <h5 class="card-title">${courseCode} ${courseNum}</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Instructor: ${selectedCourse.instrName}</li>
+              <li class="list-group-item">Location: ${selectedCourse.location}</li>
+              <li class="list-group-item">Timings: ${selectedCourse.timings}</li>
+              <li class="list-group-item"><button class="${courseCode} remove-course-btn">Remove</button></li>
+            </ul>
+          </div>
+        </div>
+      `;
+      const selectedCoursesBox = document.getElementById("selected-courses-box");
+      selectedCoursesBox.appendChild(card);
+    }
+  });
+  
