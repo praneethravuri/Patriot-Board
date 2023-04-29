72bace217f04aea4bbf4c221c2ed7a4a2b5f7e3f
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-28 23:25:19 -0400

    responsive home-page.html

--- controller/homePage.js
+++ controller/homePage.js
@@ -26,22 +26,25 @@ $(document).ready(function(){
 
 const courseCardsContainer = document.getElementById('courseCards');
 
-        studentCourses.forEach((course) => {
-            const courseDetailsObj = courseDetails[course];
-            const cardHtml = `
-                <div class="col">
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">${course}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">${courseDetailsObj.instrName}</h6>
-                            <p class="card-text">${courseDetailsObj.location}</p>
-                            <p class="card-text">${courseDetailsObj.description}</p>
-                            <p class="card-text">${courseDetailsObj.timings}</p>
-                            <p class="card-text"><button class = 'view-btn'>View Course</button></p>
-
-                        </div>
-                    </div>
+studentCourses.forEach((course) => {
+    const courseDetailsObj = courseDetails[course];
+    let courseDescription = courseDetailsObj.description;
+    if (courseDescription.length > 200) {
+        courseDescription = courseDescription.substring(0, 217) + "...";
+    }
+    const cardHtml = `
+        <div class="col">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">${course}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${courseDetailsObj.instrName}</h6>
+                    <p class="card-text">${courseDetailsObj.location}</p>
+                    <p class="card-text">${courseDescription}</p>
+                    <p class="card-text">${courseDetailsObj.timings}</p>
+                    <p class="card-text"><button class='view-btn'>View Course</button></p>
                 </div>
-            `;
-            courseCardsContainer.innerHTML += cardHtml;
-        });
+            </div>
+        </div>
+    `;
+    courseCardsContainer.innerHTML += cardHtml;
+});
